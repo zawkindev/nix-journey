2c939957c320641abd38d30313792f32075c2d4c
zawkindev <zawkindev@gmail.com>
2024-09-05 08:09:03 +0500

    feat: packaging existing sotfware

---
+++ packaging/default.nix
@@ -0,0 +1,9 @@
+# default.nix
+
+let 
+nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.05";
+pkgs = import nixpkgs { config = {}; overlays = []; };
+in
+{
+  hello = pkgs.callPackage ./hello.nix {};
+}
---
+++ packaging/hello.nix
@@ -0,0 +1,11 @@
+# hello.nix
+
+{ stdenv, fetchzip, }:
+stdenv.mkDerivation {
+  pname = "hello";
+  version = "2.12.1";
+  src = fetchzip {
+    url = "https://ftp.gnu.org/gnu/hello/hello-2.12.1.tar.gz";
+    sha256 = "1kJjhtlsAkpNB7f6tZEs+dbKd8z7KoNHyDHEJ0tmhnc=";
+  };
+}
